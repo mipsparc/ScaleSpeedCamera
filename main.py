ba245385d7653ceeff010d968e63e265067e226c
mipsparc <mipsparc@gmail.com>
2021-02-27 20:43:38 +0900

    コントラストなどを調整するバーを付けた。複数カメラに対応した

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import urllib.request
 import json
 
 # リリースバージョン
-version = 1.04
+version = 1.05
 
 # 最新バージョン確認
 try:
@@ -26,7 +26,7 @@ try:
         else:
             print('最新のバージョンです')
 except KeyboardInterrupt:
-    raise
+    sys.exit()
 except:
     pass
    
@@ -38,30 +38,30 @@ OS = platform.system()
 if OS == 'Windows':
     import win32com.client as wincl
     voice = wincl.Dispatch("SAPI.SpVoice")
-else:
-    import subprocess
-    # JPEG破損エラーを抑止する
-    @contextmanager
-    def stderr_redirected(to=os.devnull):
-        fd = sys.stderr.fileno()
 
-        ##### assert that Python and C stdio write using the same file descriptor
-        ####assert libc.fileno(ctypes.c_void_p.in_dll(libc, "stdout")) == fd == 1
+import subprocess
+import tempfile
+@contextmanager
+def stderr_redirected(to=os.devnull):
+    fd = sys.stderr.fileno()
 
-        def _redirect_stderr(to):
-            sys.stderr.close() # + implicit flush()
-            os.dup2(to.fileno(), fd) # fd writes to 'to' file
-            sys.stderr = os.fdopen(fd, 'w') # Python writes to fd
+    ##### assert that Python and C stdio write using the same file descriptor
+    ####assert libc.fileno(ctypes.c_void_p.in_dll(libc, "stdout")) == fd == 1
 
-        with os.fdopen(os.dup(fd), 'w') as old_stderr:
-            with open(to, 'w') as file:
-                _redirect_stderr(to=file)
-            try:
-                yield # allow code to be run with the redirected stdout
-            finally:
-                _redirect_stderr(to=old_stderr) # restore stdout.
-                                                # buffering and flags such as
-                                                # CLOEXEC may be different
+    def _redirect_stderr(to):
+        sys.stderr.close() # + implicit flush()
+        os.dup2(to.fileno(), fd) # fd writes to 'to' file
+        sys.stderr = os.fdopen(fd, 'w') # Python writes to fd
+
+    with os.fdopen(os.dup(fd), 'w') as old_stderr:
+        with open(to, 'w') as file:
+            _redirect_stderr(to=file)
+        try:
+            yield # allow code to be run with the redirected stdout
+        finally:
+            _redirect_stderr(to=old_stderr) # restore stdout.
+                                            # buffering and flags such as
+                                            # CLOEXEC may be different
 
 def speak(speech_text):
     if OS == 'Windows':
@@ -69,20 +69,68 @@ def speak(speech_text):
     else:
         subprocess.run(f"echo '{speech_text}' | open_jtalk -x /var/lib/mecab/dic/open-jtalk/naist-jdic -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice -ow /dev/stdout | aplay --quiet", shell=True)
 
-camera_id = 0
+def show(cv, frame):
+    cv.imshow('ScaleSpeedCamera',frame)
+    if cv.waitKey(1) & 0xFF == ord('q') or cv.getWindowProperty('ScaleSpeedCamera', 0) == -1:
+        print('終了します')
+        sys.exit()
+        
+def changeContrast(num):
+    cap.set(cv2.CAP_PROP_CONTRAST, num)
+    
+def changeSpeedParam(num):
+    global weight
+    weight = (num + 1) / 10.0
+
+def changeRectSize(num):
+    global rect_size
+    rect_size = num
+
+camera_id_max = -1
+for camera_id in range(4, -1, -1):
+    with stderr_redirected():
+        cap = cv2.VideoCapture(camera_id)
+        if cap.isOpened():
+            camera_id_max = camera_id
+            cap.release()
+            break
+
+if camera_id_max < 0:
+    print('カメラが検出できませんでした。')
+    sys.exit()
+    
+if camera_id_max > 0:
+    print('複数のカメラが検出されました。どのカメラを使いますか?"')
+    for i in range(camera_id_max + 1):
+        print(f'カメラID: {i}')
+    camera_id_selected = int(input('カメラID(数字)を入力してEnter> '))
+else:
+    camera_id_selected = camera_id_max
+    
+if OS == 'Windows':
+    cap = cv2.VideoCapture(camera_id_selected + cv2.CAP_DSHOW)
+else:
+    cap = cv2.VideoCapture(camera_id_selected)
+
 camera_width = 1280
 camera_height = 720
 camera_fps = 30
-
-# Integrated Camera Lenovo ThinkPadでテスト済みの設定
-cap = cv2.VideoCapture(camera_id)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, camera_width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, camera_height)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
 cap.set(cv2.CAP_PROP_FPS, camera_fps)
-cap.set(cv2.CAP_PROP_CONTRAST, 150)
-
 print('起動しました')
+print()
+
+cv2.namedWindow('ScaleSpeedCamera')
+changeContrast(80)
+cv2.createTrackbar('Contrast', 'ScaleSpeedCamera', 80 , 300, changeContrast)
+changeSpeedParam(4)
+cv2.createTrackbar('SpeedParam', 'ScaleSpeedCamera', 3 , 8, changeSpeedParam)
+changeRectSize(150)
+cv2.createTrackbar('MinRectSize', 'ScaleSpeedCamera', 150 , 300, changeRectSize)
+
+last_kph = None
 
 def MeasureSpeed(cap):
     a_center = None
@@ -93,6 +141,9 @@ def MeasureSpeed(cap):
     passed_b_time = None
     cnt_qr = 0
     last_time = 0
+    global last_kph
+    global weight
+    global rect_size
     
     # 列車が去るまで(rectがなくなるまで)なにもしない。30フレーム数える
     is_still = 30
@@ -116,9 +167,9 @@ def MeasureSpeed(cap):
                 if qr_save_cnt <= 0:
                     a_center = None
                     b_center = None
-                    qr_save_cnt = 30
+                    qr_save_cnt = 40
             else:
-                qr_save_cnt = 30
+                qr_save_cnt = 40
 
             scale = 'N'
             for d in qrdata:
@@ -148,18 +199,16 @@ def MeasureSpeed(cap):
 
         # QRコードが認識できなかった場合
         if not (a_center and b_center):
-            cv2.imshow('ScaleSpeedCamera',frame)
-            if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty('ScaleSpeedCamera', 0) == -1:
-                raise KeyboardInterrupt
+            show(cv2, frame)
             continue
 
         if avg is None:
             avg = frame.copy().astype("float")
             continue
 
-        cv2.accumulateWeighted(frame, avg, 0.4)
+        cv2.accumulateWeighted(frame, avg, weight)
         frameDelta = cv2.absdiff(frame, cv2.convertScaleAbs(avg))
-        thresh = cv2.threshold(frameDelta, 20, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.threshold(frameDelta, 40, 255, cv2.THRESH_TOZERO)[1]
         
         contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
@@ -168,7 +217,7 @@ def MeasureSpeed(cap):
         for i in range(0, len(contours)):
             if len(contours[i]) > 0:
                 # 小さいオブジェクトを除去する
-                if cv2.contourArea(contours[i]) < 150:
+                if cv2.contourArea(contours[i]) < rect_size:
                     continue
 
                 rect = contours[i]
@@ -182,6 +231,8 @@ def MeasureSpeed(cap):
                 
                 #線路の微妙な部分を排除する
                 if h < 10:
+                    continue
+                if w > 100:
                     continue
                 
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
@@ -199,26 +250,26 @@ def MeasureSpeed(cap):
                 if a_center < max_x < (a_center + b_center) / 2:
                     train_from = 'left'
                     passed_a_time = time.time()
-                    print('from left')
+                    print('列車が左から来ました')
                 elif (a_center + b_center) / 2 < min_x < b_center:
                     train_from = 'right'
                     passed_b_time = time.time()
-                    print('from right')
+                    print('列車が右から来ました')
             
             if train_from == 'left' and passed_a_time + 0.5 < time.time():
                 if passed_b_time is None:
                     if max_x > b_center:
-                        print('passed right')
+                        print('右を通過しました')
                         passed_b_time = time.time()
             elif train_from == 'right' and passed_b_time + 0.5 < time.time():
                 if passed_a_time is None:
                     if a_center > min_x:
-                        print('passed left')
+                        print('左を通過しました')
                         passed_a_time = time.time()
                             
-            if passed_a_time and (time.time() > passed_a_time + 6):
+            if passed_a_time and (time.time() > passed_a_time + 7):
                 break
-            if passed_b_time and (time.time() > passed_b_time + 6):
+            if passed_b_time and (time.time() > passed_b_time + 7):
                 break
         else:
             is_still -= 1
@@ -234,21 +285,18 @@ def MeasureSpeed(cap):
                 kph = int((qr_length / passing_time) * 3.6 * 220)
             print(f'時速{kph}キロメートルです')
             speak(f'時速{kph}キロメートルです')
+            last_kph = kph
             break
-        
+
+        if last_kph:
+            text_area =  cv2.getTextSize(f'{last_kph}km/h', cv2.FONT_HERSHEY_DUPLEX, 3, 3)[0]
+            cv2.rectangle(frame, (0, 0), (text_area[0] + 70, text_area[1] + 40), (150, 150 , 150), -1)
+            cv2.putText(frame, f'{last_kph}km/h', (35, text_area[1] + 20), cv2.FONT_HERSHEY_DUPLEX, 3, (0, 255, 0), 3)
         cv2.line(frame, (a_bottom_center, a_bottom_center_y), (a_bottom_center, 0), (255, 0, 0), 3)
         cv2.line(frame, (b_bottom_center, b_bottom_center_y), (b_bottom_center, 0), (255, 0, 0), 3)
         cv2.line(frame, (0, a_top), (2000, b_top), (255, 0, 0), 3)
         cv2.line(frame, (0, a_top - 300), (2000, b_top - 300), (255, 0, 0), 3)
-        
-        cv2.imshow('ScaleSpeedCamera',frame)
+        show(cv2, frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty('ScaleSpeedCamera', 0) == -1:
-            raise KeyboardInterrupt
-        
-try:
-    while (cap.isOpened()):
-        MeasureSpeed(cap)
-finally:
-    cap.release()
-    cv2.destroyAllWindows()
+while (cap.isOpened()):
+    MeasureSpeed(cap)
